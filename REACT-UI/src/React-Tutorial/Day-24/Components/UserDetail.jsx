302bb4d53e95f9c5bffc44762bd83236cb6f6014
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-09-26 10:56:06 +0530

    .

--- REACT-UI/src/React-Tutorial/Day-24/Components/UserDetail.jsx
+++ REACT-UI/src/React-Tutorial/Day-24/Components/UserDetail.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 const UserDetail = () => {
     const params = useParams()
     const userId = params.userId
+    // Using Destructuring 
+    // const { userId } = useParams()
     return (
         <div>Detail of User {userId}</div>
     )

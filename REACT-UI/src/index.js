791d400d3b626f9b5833fedb3637fbc3cf49485d
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-10-02 16:52:26 +0530

    .

--- REACT-UI/src/index.js
+++ REACT-UI/src/index.js
@@ -10,14 +10,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 
 // import App from './React-Tutorial/Day-24/App';
-import NestedRouteApp from './React-Tutorial/Day-24/NestedRouteApp';
+// import NestedRouteApp from './React-Tutorial/Day-24/NestedRouteApp';
+import { Body } from './Netflix-Clone/Body.component';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-  <NestedRouteApp />
+  <Body />
 
 );
 
